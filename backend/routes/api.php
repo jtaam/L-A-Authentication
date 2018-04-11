a5fa46f7f328b8a9cb4d0036ab5c4a7de187fa16
dev-tam <tamjid.mdh@gmail.com>
2018-04-11 23:22:01 +0600

    JWT config

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +27,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
